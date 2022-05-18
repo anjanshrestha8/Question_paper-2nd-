45d1b1d35be0431fce2ecb2c29cd3b8f88b32663
Mansan760 <mansansilwal@gmail.com>
2022-05-18 14:36:37 +0545

    added a file

--- filehandelling_1.cpp
+++ filehandelling_1.cpp
@@ -3,5 +3,42 @@
 //open a file
 //read a file
 //delete file
+/*#include<iostream>
+#include<fstream>
+using namespace std;
+int main(){
+	fstream FileName ;
+	FileName.open("D:\OVER.txt",ios::out);
+	if(!FileName){
+		 cout<<"File creation failed";
+    }
+    else
+    {
+        cout<<"New file created";
+        FileName<<"school";
+        FileName.close(); 
+    }
+    return 0;
+}*/
 #include<iostream>
 #include<fstream>
+using namespace std;
+int main(){
+	fstream a;
+	a.open("D:\OVER.txt",ios::in);
+		if(!a){
+		 cout<<"No such file";
+    }
+    else {
+    	char ch;
+    	while(1){
+    		OVER>>ch;
+    		cout<<ch;
+    		if(OVER.eof){
+    			break;
+			}
+		}
+	}
+	a.close();return 0;
+	
+}
